23d26ab9cc111e7882822b6c28ffb6813d9d44a8
lvdianzhao <lv_workwith@163.com>
2021-10-25 23:56:13 +0800

    创建项目

---
+++ front-project/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Button } from "antd";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
